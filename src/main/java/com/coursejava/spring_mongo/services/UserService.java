0cfd5d74cd7cc9f1d82c1331cc6d8c1d5476f4d5
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-19 15:58:02 -0300

    inserindo usuário post

--- src/main/java/com/coursejava/spring_mongo/services/UserService.java
+++ src/main/java/com/coursejava/spring_mongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.coursejava.spring_mongo.services;
 
 import com.coursejava.spring_mongo.domain.User;
+import com.coursejava.spring_mongo.dto.UserDTO;
 import com.coursejava.spring_mongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+
+    public User insert(User user) {
+        return repository.save(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
