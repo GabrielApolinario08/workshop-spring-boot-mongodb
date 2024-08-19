256665cbace92d5b074a46e1820248f990b6d5a8
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-19 15:01:33 -0300

    criando userDTO

--- src/main/java/com/coursejava/spring_mongo/resources/UserResource.java
+++ src/main/java/com/coursejava/spring_mongo/resources/UserResource.java
@@ -1,11 +1,11 @@
 package com.coursejava.spring_mongo.resources;
 
 import com.coursejava.spring_mongo.domain.User;
+import com.coursejava.spring_mongo.dto.UserDTO;
 import com.coursejava.spring_mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +17,9 @@ public class UserResource {
     @Autowired
     private UserService service;
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = service.findAll();
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> userDTOS = users.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(userDTOS);
     }
 }
