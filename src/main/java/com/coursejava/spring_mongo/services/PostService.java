a9eaef66e7646155a20c6deef3b4a9c33875623a
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-20 15:21:39 -0300

    criando método findById para posts

---
+++ src/main/java/com/coursejava/spring_mongo/services/PostService.java
@@ -0,0 +1,19 @@
+package com.coursejava.spring_mongo.services;
+
+import com.coursejava.spring_mongo.domain.Post;
+import com.coursejava.spring_mongo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
