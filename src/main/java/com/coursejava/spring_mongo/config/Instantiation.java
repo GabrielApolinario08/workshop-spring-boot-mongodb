341fb9d36a3f7b16e996caac878d596ae6c8bf7b
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-20 15:03:49 -0300

    retornando posts de um usuário

--- src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
+++ src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
@@ -33,5 +33,8 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, Instant.parse("2018-03-21T12:00:00Z"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, Instant.parse("2018-03-23T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
