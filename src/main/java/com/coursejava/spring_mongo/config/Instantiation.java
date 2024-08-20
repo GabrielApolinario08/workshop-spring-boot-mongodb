e16f483547ec6399b68aa29cc22fdb788cb898d8
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-20 14:53:00 -0300

    criando AuthorDTO

--- src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
+++ src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
@@ -2,6 +2,8 @@ package com.coursejava.spring_mongo.config;
 
 import com.coursejava.spring_mongo.domain.Post;
 import com.coursejava.spring_mongo.domain.User;
+import com.coursejava.spring_mongo.dto.AuthorDTO;
+import com.coursejava.spring_mongo.dto.UserDTO;
 import com.coursejava.spring_mongo.repository.PostRepository;
 import com.coursejava.spring_mongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +29,9 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, Instant.parse("2018-03-21T12:00:00Z"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, Instant.parse("2018-03-23T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", maria);
-        postRepository.saveAll(Arrays.asList(post1, post2));
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, Instant.parse("2018-03-21T12:00:00Z"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, Instant.parse("2018-03-23T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
