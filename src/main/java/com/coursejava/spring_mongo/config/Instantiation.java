cfa2bcb781245040f3c5607a47088c21ff97cc66
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-20 15:39:24 -0300

    acrescentando comentários aos posts

--- src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
+++ src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.coursejava.spring_mongo.config;
 import com.coursejava.spring_mongo.domain.Post;
 import com.coursejava.spring_mongo.domain.User;
 import com.coursejava.spring_mongo.dto.AuthorDTO;
+import com.coursejava.spring_mongo.dto.CommentDTO;
 import com.coursejava.spring_mongo.dto.UserDTO;
 import com.coursejava.spring_mongo.repository.PostRepository;
 import com.coursejava.spring_mongo.repository.UserRepository;
@@ -32,6 +33,11 @@ public class Instantiation implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         Post post1 = new Post(null, Instant.parse("2018-03-21T12:00:00Z"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, Instant.parse("2018-03-23T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        CommentDTO commentDTO1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T12:00:00Z"), new AuthorDTO(alex));
+        CommentDTO commentDTO2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-22T12:00:00Z"), new AuthorDTO(bob));
+        CommentDTO commentDTO3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-03-23T12:00:00Z"), new AuthorDTO(alex));
+        post1.getComments().addAll(Arrays.asList(commentDTO1, commentDTO2));
+        post2.getComments().add(commentDTO3);
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/com/coursejava/spring_mongo/domain/Post.java
+++ src/main/java/com/coursejava/spring_mongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.coursejava.spring_mongo.domain;
 
 import com.coursejava.spring_mongo.dto.AuthorDTO;
+import com.coursejava.spring_mongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
     public Post() {
     }
 
@@ -66,6 +70,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
