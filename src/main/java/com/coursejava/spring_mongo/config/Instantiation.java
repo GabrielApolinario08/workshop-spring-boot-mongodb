e64ec58a8ebe6ed7a85ce2b95e7561abeca1d7fb
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-20 14:42:21 -0300

    criando entity post com user aninhado e camada repository

--- src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
+++ src/main/java/com/coursejava/spring_mongo/config/Instantiation.java
@@ -1,25 +1,36 @@
 package com.coursejava.spring_mongo.config;
 
+import com.coursejava.spring_mongo.domain.Post;
 import com.coursejava.spring_mongo.domain.User;
+import com.coursejava.spring_mongo.repository.PostRepository;
 import com.coursejava.spring_mongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
 
 
     @Override
     public void run(String... args) throws Exception {
-        repository.deleteAll();
+        postRepository.deleteAll();
+        userRepository.deleteAll();
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        repository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, Instant.parse("2018-03-21T12:00:00Z"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, Instant.parse("2018-03-23T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", maria);
+        postRepository.saveAll(Arrays.asList(post1, post2));
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
     }
 }
